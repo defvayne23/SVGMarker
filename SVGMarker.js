943edeaea76791b34e0c36b144215a1807d2bb55
John Hoover <defvayne23@gmail.com>
2016-06-19 20:55:21 -0500

    Add draggable functionality.
    Change how options are stored.

--- SVGMarker.js
+++ SVGMarker.js
@@ -5,6 +5,8 @@ SVGMarker.prototype = new google.maps.OverlayView();
 function SVGMarker(options) {
   var default_ = {
     clickable: true,
+    cursor: 'pointer',
+    draggable: false,
     icon: {
       anchor: new google.maps.Point(0, 0),
       size: new google.maps.Size(30,30),
@@ -40,7 +42,7 @@ function SVGMarker(options) {
       }
     }
   }
-  this.options_ = default_;
+  this.setValues(default_);
 
   // Define div container
   this.div_ = null;
@@ -59,94 +61,127 @@ SVGMarker.prototype.onAdd = function() {
   var self = this;
 
   // Create container with no visible styles
-  this.div_ = document.createElement('div');
-  this.div_.style.border = 'none';
-  this.div_.style.borderWidth = '0px';
-  this.div_.style.position = 'absolute';
+  var container = document.createElement('div');
+  container.style.border = 'none';
+  container.style.borderWidth = '0px';
+  container.style.position = 'absolute';
 
   // Set if marker is visible when added
-  if(this.options_.visible === false) {
-    this.div_.style.visibility = 'hidden';
+  if(this.get('visible') === false) {
+    container.style.visibility = 'hidden';
   }
 
   // Set marker opacity
-  if(this.options_.opacity >= 0 && this.options_.opacity <= 1) {
-    this.div_.style.opacity = this.options_.opacity;
+  if(this.get('opacity') >= 0 && this.get('opacity') <= 1) {
+    container.style.opacity = this.get('opacity');
   }
 
   // Add click event to container
-  if(this.options_.clickable === true) {
-    google.maps.event.addDomListener(this.div_, 'click', function(event) {
+  if(this.get('clickable') === true) {
+    google.maps.event.addDomListener(container, 'click', function(event) {
       google.maps.event.trigger(self, 'click', event);
     });
   }
 
   // Add drag start and end to container
-  if(this.options_.draggable === true) {
-    // TODO: Add draggable
+  if(this.get('draggable') === true) {
+    google.maps.event.addDomListener(container, 'drag', function(event) {
+      google.maps.event.trigger(self, 'drag', event);
+    });
+
+    google.maps.event.addDomListener(container, 'mousedown', function(event){
+      google.maps.event.trigger(self, 'dragstart', event);
+
+      this.style.cursor='move';
+      self.get('map').set('draggable',false);
+
+      var origin = event;
+
+      self.moveHandler  = google.maps.event.addDomListener(self.get('map').getDiv(), 'mousemove', function(event){
+        google.maps.event.trigger(self, 'drag', event);
+
+        var left   = origin.clientX-event.clientX,
+            top    = origin.clientY-event.clientY,
+            pos    = self.getProjection().fromLatLngToDivPixel(self.get('position')),
+            latLng = self.getProjection().fromDivPixelToLatLng(new google.maps.Point(pos.x-left,pos.y-top));
+
+        origin = event;
+        self.set('position', latLng);
+        self.draw();
+      });
+    });
+
+    google.maps.event.addDomListener(container, 'mouseup', function(){
+      google.maps.event.trigger(self, 'dragend', event);
+      self.get('map').set('draggable',true);
+      this.style.cursor='default';
+      google.maps.event.removeListener(self.moveHandler);
+    });
+
+    google.maps.event.addDomListener(this.get('map').getDiv(), 'mouseleave', function(){
+      google.maps.event.trigger(container, 'mouseup');
+    });
   }
 
   // Create the img element
   var img = document.createElement('img');
-  img.src = this.options_.icon.url;
-  img.style.width = this.options_.icon.size.width+'px';
-  img.style.height = this.options_.icon.size.height+'px';
-  img.setAttribute('alt', this.options_.title);
+  img.src = this.get('icon').url;
+  img.style.width = this.get('icon').size.width+'px';
+  img.style.height = this.get('icon').size.height+'px';
+  img.setAttribute('alt', this.get('title'));
 
   // Attach image to div
-  this.div_.appendChild(img);
+  container.appendChild(img);
 
-  if(this.options_.icon.text.content !== '') {
+  if(this.get('icon').text.content !== '') {
     var text = document.createElement('span');
-    text.textContent = this.options_.icon.text.content;
-    text.style.fontFamily = this.options_.icon.text.font;
-    text.style.fontSize = this.options_.icon.text.size;
-    text.style.fontWeight = this.options_.icon.text.weight;
-    text.style.color = this.options_.icon.text.color;
+    text.textContent = this.get('icon').text.content;
+    text.style.fontFamily = this.get('icon').text.font;
+    text.style.fontSize = this.get('icon').text.size;
+    text.style.fontWeight = this.get('icon').text.weight;
+    text.style.color = this.get('icon').text.color;
     text.style.position = 'absolute';
-    text.style.top = this.options_.icon.text.position[0] + 'px';
-    text.style.left = this.options_.icon.text.position[1] + 'px';
+    text.style.top = this.get('icon').text.position[0] + 'px';
+    text.style.left = this.get('icon').text.position[1] + 'px';
     text.style.transform = 'translate(-50%, -50%)';
-    this.div_.appendChild(text);
+    this.get('container').appendChild(text);
   }
 
   // Add the element to the "overlayImage" pane.
-  var panes = this.getPanes();
-  panes.overlayImage.appendChild(this.div_);
+  this.set('container', container);
+  this.getPanes().overlayImage.appendChild(container);
 };
 
 SVGMarker.prototype.draw = function() {
   var overlayProjection = this.getProjection();
-  var coords = overlayProjection.fromLatLngToDivPixel(this.options_.position);
+  var coords = overlayProjection.fromLatLngToDivPixel(this.get('position'));
 
-  this.div_.style.left = (coords.x - this.options_.icon.anchor.x) + 'px';
-  this.div_.style.top = (coords.y - this.options_.icon.anchor.y) + 'px';
+  this.get('container').style.left = (coords.x - this.get('icon').anchor.x) + 'px';
+  this.get('container').style.top = (coords.y - this.get('icon').anchor.y) + 'px';
 
-  this.div_.style.cursor = 'pointer'; // TODO: Only set this if clickable. Change if draggable.
+  this.get('container').style.cursor = this.get('cursor');
 };
 
 SVGMarker.prototype.onRemove = function() {
-  this.div_.parentNode.removeChild(this.div_);
-  this.div_ = null;
+  this.get('container').parentNode.removeChild(this.get('container'));
+  this.set('container', null);
 };
 
-// Set the visibility to 'hidden' or 'visible'.
 SVGMarker.prototype.hide = function() {
-  if (this.div_) {
-    // The visibility property must be a string enclosed in quotes.
-    this.div_.style.visibility = 'hidden';
+  if (this.get('container')) {
+    this.get('container').style.visibility = 'hidden';
   }
 };
 
 SVGMarker.prototype.show = function() {
   if (this.div_) {
-    this.div_.style.visibility = 'visible';
+    this.get('container').style.visibility = 'visible';
   }
 };
 
 SVGMarker.prototype.toggle = function() {
   if (this.div_) {
-    if (this.div_.style.visibility === 'hidden') {
+    if (this.get('container').style.visibility === 'hidden') {
       this.show();
     } else {
       this.hide();
@@ -155,7 +190,7 @@ SVGMarker.prototype.toggle = function() {
 };
 
 SVGMarker.prototype.getPosition = function() {
-  return this.options_.position;
+  return this.get('position');
 };
 
 // Detach from map
@@ -165,6 +200,6 @@ SVGMarker.prototype.toggleDOM = function() {
     this.setMap(null);
   } else {
     // Note: Calls onAdd()
-    this.setMap(this.options_.map);
+    this.setMap(this.get('map'));
   }
 };
