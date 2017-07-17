a3a270e75d79850f023baa9867f674d319ffc088
John Hoover <defvayne23@gmail.com>
2017-07-17 15:06:23 -0500

    Version 0.7.0

--- SVGMarker.js
+++ SVGMarker.js
@@ -1,4 +1,4 @@
-/*! SVGMarker v0.6.0 - https://github.com/defvayne23/SVGMarker */
+/*! SVGMarker v0.7.0 - https://github.com/defvayne23/SVGMarker */
 SVGMarker.prototype = new google.maps.OverlayView();
 
 /** @constructor */
